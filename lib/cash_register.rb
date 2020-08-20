30b2420c4ef53cb08219019e9ee6e0e0fb1d9c60
Marques Avery <averymarques4@gmail.com>
2020-08-20 20:36:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
   def add_item(item, price, quantity = 1)
     new_price = price * quantity
     @total += new_price
-    @item_list.fill(item, array.size, quantity)
+    @item_list.fill(item, item_list.size, quantity)
   end
 
   def apply_discount
