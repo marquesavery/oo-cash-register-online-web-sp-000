89ca2cf76c4ff90de76b528126d84cc7b7f2b119
Marques Avery <averymarques4@gmail.com>
2020-08-20 20:17:07 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,7 +5,7 @@ class CashRegister
 
   def initialize(discount = 0)
     @total = 0
-    @discount = discount
+    @discount = number_to_percentage(discount)
   end
 
   def add_item(item, price, quantity = 1)
