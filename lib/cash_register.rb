c8f9c536acac9108a1b6bd7b4e5d74b0c5f8bb12
Marques Avery <averymarques4@gmail.com>
2020-08-20 19:58:31 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@ class CashRegister
 
   attr_accessor :total, :employee_discount
 
-  def initialize(employee_discount = 0)
+  def initialize(employee_discount == 0)
     @total = 0
     @employee_discount = employee_discount
   end
